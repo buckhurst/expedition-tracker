e25e22864ba2684ab620bbf8f2ae33a7e87b8756
Nicola Buckhurst <nicola@buckhurst.org>
2012-10-30 21:24:10 +0000

    Created rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ExpeditionTracker::Application.config.secret_token = '9066d267a26144e6cb4c04a2a5e310e4595b49da851ac6a6b846325de6a262f20b76765a802d6af4fb079b425589e2a3199f4a59a9a2bad5a9f2ff750079b538'
